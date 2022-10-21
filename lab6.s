2d32225b620af5a99ff3d8d5a0967ce71dfa3458
João Pedro Carolino Morais <96194625+jpcmorais16@users.noreply.github.com>
2022-10-20 22:55:38 -0300

    Add files via upload

--- lab6.s
+++ lab6.s
@@ -1,16 +1,14 @@
 .globl _start
 
-
-
-
 _start:
 
     jal open
     jal read
 
-    la t0, input_adress #bytes 3 e 5 sao largura e altura
-    li t1, 0 #largura
-    li t2, 0 #altura
+    la a6, input_adress #bytes 3 e 5 sao largura e altura
+    jal getDimensions
+    #li a4, 0 #largura
+    #li a5, 0 #altura
     li t3, 24
     li t5, 7
     #lw t3, 3(input_adress)
@@ -25,47 +23,44 @@ _start:
     li a7, 2201
     ecall   
 
-1:
+loop_altura:
 
-2:
-    mul t6, t1, t2 # altura e largura
-    addi t6, t6, 8 # soma 8 
-    add t6, t6, t0 # soma o endereco de inicio do arquivo
+    loop_largura:
+        addi a6, a6, 1
 
-    mv a0, t1 # x atual da imagem
-    mv a1, t2 # y atual da imagem
+        mv a0, a4 # x atual da imagem
+        mv a1, a5 # y atual da imagem
 
-    # a2
-    #===================
-    li a2, 0
+        # a2
+        #===================
+        li a2, 0
 
-    lb t4, 0(t6)
-    sll t4, t4, 24
-    or a2, a2, t4
+        lb t4, 0(a6)
+        sll t4, t4, 24
+        or a2, a2, t4
 
-    lb t4, 0(t6)
-    sll t4, t4, 16
-    or a2, a2, t4
+        lb t4, 0(a6)
+        sll t4, t4, 16
+        or a2, a2, t4
 
-    lb t4, 0(t6)
-    sll t4, t4, 8
-    or a2, a2, t4
+        lb t4, 0(a6)
+        sll t4, t4, 8
+        or a2, a2, t4
 
-    li t4, 255
-    or a2, a2, t4
+        li t4, 255
+        or a2, a2, t4
 
-    #===================
+        #===================
 
-    jal setPixel
+        jal setPixel
 
-    addi t1, t1, 1
-    bne t1, t3, 2b # loop de largura 
+        addi a4, a4, 1
+        bne a4, t5, loop_largura # loop de largura 
 
-2:
-    addi t2, t2, 1
-    bne t2, t5, 1b # loop de altura
 
-1:
+    addi a5, a5, 1
+    bne a5, t3, loop_altura # loop de altura
+
     
 
 open:
@@ -91,7 +86,64 @@ setPixel:
     ecall
     ret
 
+getDimensions:
+    li t1, '\n'
+    li t2, ' '
+    li t3, 0 # contador de \n e espaco
+    li t4, 3 # maximo de \n e espaco
+    li t5, 10 # DEZ
+    li t6, 1 # UM
+    li t0, 2 # DOIS
+    
+    li a4, 0
+    li a5, 0
 
+    addi a6, a6, 3
+
+    1:
+        lbu t6, 0(a6)
+
+        beq t3, t4, 5f #ACABA O LOOP
+
+        beq t1, t6, 4f # PULAM CASO SEJA \N OU ESPACO
+        beq t2, t6, 4f #
+
+        beq t3, t6, 2f # PULA PARA SOMAR NA LARGURA
+        beq t3, t0, 3f # PULA PARA SOMAR NA LARGURA
+
+
+        addi t6, t6, 1 # SOMA 1 NO ENDERECO DA IMAGEM
+        j 1b # REINICIA O LOOP
+    
+        2: #pegar a largura
+
+            mul a4, t6, t5 # MULTIPLICA POR 10
+            addi a4, t6, -'0' # CONVERTE O BYTE DE ASCII PARA O NUMERO
+
+            addi t6, t6, 1 # SOMA 1 NO ENDERECO DA IMAGEM
+            j 1b # REINICIA O LOOP
+            
+        3: #pegar a altura
+
+            mul a5, t6, t5 # MULTIPLICA POR 10
+            addi a5, t6, -'0' # CONVERTE O BYTE DE ASCII PARA O NUMERO
+
+            addi t6, t6, 1 # SOMA 1 NO ENDERECO DA IMAGEM
+            j 1b # REINICIA O LOOP
+
+
+        4: #if o caracter eh espaco ou \n
+
+            addi t3, t3, 1
+
+            addi t6, t6, 1 # SOMA 1 NO ENDERECO DA IMAGEM
+            j 1b # REINICIA O LOOP
+
+        5: #if o contador chegou a 3
+        
+    addi a6, a6, 1
+
+    ret
     
 
     
